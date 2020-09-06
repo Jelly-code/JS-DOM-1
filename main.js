4df0b3a3cf2ad05c9b3fc43500941a61a70cd32c
Jelly-code <santamaria.javiera@gmail.com>
2020-09-06 17:02:20 -0300

    Playlist listo

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
+//CANCION 1
 let cancionUsuario1 = prompt('Escribe tu canción favorita')
-let cancionUsuario2 = prompt('Escribe tu canción favorita')
-let cancionUsuario3 = prompt('Escribe tu canción favorita')
-let cancionUsuario4 = prompt('Escribe tu canción favorita')
-
 const cancion1 = document.querySelector('#cancion1')
-cancion1.textContent = respuestaUsuario1
+cancion1.textContent = cancionUsuario1
 
+//CANCION 2
+let cancionUsuario2 = prompt('Escribe tu canción favorita')
 const cancion2 = document.querySelector('#cancion2')
-cancion2.textContent = respuestaUsuario2
+cancion2.textContent = cancionUsuario2
 
+//CANCION 3
+let cancionUsuario3 = prompt('Escribe tu canción favorita')
 const cancion3 = document.querySelector('#cancion3')
-cancion3.textContent = respuestaUsuario3
+cancion3.textContent = cancionUsuario3
 
+//CANCION 4
+let cancionUsuario4 = prompt('Escribe tu canción favorita')
 const cancion4 = document.querySelector('#cancion4')
-cancion4.textContent = respuestaUsuario4
+cancion4.textContent = cancionUsuario4
