3c4d0fcd04dbfb4b0ece671ce7e4a11d7811a29c
Jelly-code <santamaria.javiera@gmail.com>
2020-09-06 16:55:08 -0300

    JS

---
+++ main.js
@@ -0,0 +1,16 @@
+let cancionUsuario1 = prompt('Escribe tu canción favorita')
+let cancionUsuario2 = prompt('Escribe tu canción favorita')
+let cancionUsuario3 = prompt('Escribe tu canción favorita')
+let cancionUsuario4 = prompt('Escribe tu canción favorita')
+
+const cancion1 = document.querySelector('#cancion1')
+cancion1.textContent = respuestaUsuario1
+
+const cancion2 = document.querySelector('#cancion2')
+cancion2.textContent = respuestaUsuario2
+
+const cancion3 = document.querySelector('#cancion3')
+cancion3.textContent = respuestaUsuario3
+
+const cancion4 = document.querySelector('#cancion4')
+cancion4.textContent = respuestaUsuario4
